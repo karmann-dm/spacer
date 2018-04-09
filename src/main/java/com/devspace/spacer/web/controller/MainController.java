d689764b11bff89e8cf11b6c58f3cbe52659bc46
Karmanov Dmitry <karmanno@gmail.com>
2018-04-09 17:06:50 +1000

    Created main endpoint with login route.

---
+++ src/main/java/com/devspace/spacer/web/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.devspace.spacer.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+}
