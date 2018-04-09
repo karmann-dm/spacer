251e8127b3d7e2360c398cd3d91cd1a748f78d4e
Karmanov Dmitry <karmanno@gmail.com>
2018-04-09 13:16:26 +1000

    Created user jpa repository.

---
+++ src/main/java/com/devspace/spacer/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.devspace.spacer.repository;
+
+import com.devspace.spacer.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
